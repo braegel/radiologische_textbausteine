b169ce689f2c1580ed6fd65cf77ca92e537aa6df
Bernd Brägelmann <bernd@braegelmann.net>
2023-06-05 20:46:42 +0200

    switching to report class

--- yaml2txt.py
+++ yaml2txt.py
@@ -1,5 +1,30 @@
 import yaml
 
+class Report:
+    def __init__(self, modality,region,shortcut,history,technique,findings): #,region,shortcut,history,technique,findings):
+        self.modality=modality
+        self.region=region
+        self.shortcut=shortcut
+        self.history=history
+        self.technique=technique
+        self.findings=findings
+
+    def de_txt(self):
+        print ("* ", end ='')
+        if self.modality == 'cr':
+            print("Röntgen ", end = '')
+        print(self.region+": "+self.shortcut)
+        print("Klinische Informationen: "+self.history)
+        print();
+        print("Technik: "+self.technique)
+        if self.modality=='cr':
+            print("Die rechtfertigende Indikation gemäß StrSchV §119 wurde durch die im Strahlenschutz fachkundige unterzeichende Person gestellt.")
+        print()
+        print("Befund")
+        print()
+        print("Beurteilung")
+        print()
+
 def read_yaml(file_path):
     with open(file_path, 'r') as stream:
         try:
@@ -20,14 +45,28 @@ def data_txt(data):
                         print("* " + "Röntgen "+region['region']+": "+region['shortcut'])
                     print("Rechtfertigende Indikation: " + region['history']) # TODO for MRI
                     print()
+                    print("Technik: " + region['technique'])
+                    print()
                     print("Befund:")
-                    for finding in region['findings']: 
-                        print(str(finding)+": ") # TODO current
+                    for finding_cat in region['findings']:
+                        print (f"{finding_cat}")
+                    print();
+                    print("Beurteilung: \n")
+
+def parse_yaml(data):
+    for modality in data['modality']:
+        if isinstance(modality, dict):
+            for regions in modality.values():
+                for region in regions:
+                    report=Report(list(modality)[0], region['region'], region['shortcut'], region['history'], region['technique'],region['findings'])
+                    report.de_txt()
+
 def main():
-    yaml_file = 'textbausteine.yaml'  # your yaml file path
+    yaml_file = 'textbausteine.yaml'  # your yaml file path 
     txt_file = 'textbausteine.txt'  # your txt file path
     data = read_yaml(yaml_file)
-    data_txt(data)
+    list_of_reports=parse_yaml(data)
+#    data_txt(data)
 
 if __name__ == "__main__":
     main()
